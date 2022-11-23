14b306d18145996d2cc9ed305bad8adc76f4cc89
Bharath Gona <108396130+bharath1399@users.noreply.github.com>
2022-11-23 16:27:45 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,29 @@
+let shopItemsData =[{
+    id :"shghhh",
+    name:"Casual Shirt",
+    price: 45,
+    desc: "lorem ipsum dolor,sit amet consectetur adipiscing",
+    img: "images/img-1.jpg"
+
+},{ 
+    id :"grrhg",
+    name:"Office Shirt",
+    price: 100,
+    desc: "lorem ipsum dolor,sit amet consectetur adipiscing",
+    img: "images/img-2.jpg"
+
+},{
+    id :"gfnfnnb",
+    name:"Tshirt",
+    price: 25,
+    desc: "lorem ipsum dolor,sit amet consectetur adipiscing",
+    img: "images/img-3.jpg"
+
+},{ 
+    id :"nfnfnn",
+    name:"Mens Suit",
+    price: 300,
+    desc: "lorem ipsum dolor,sit amet consectetur adipiscing",
+    img: "images/img-4.jpg"
+
+}];
